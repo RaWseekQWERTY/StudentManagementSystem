f1cc72728fd54d9be0b52df6c89f037ee84df428
RaWseekQWERTY <rasikkayasthav@gmail.com>
2024-03-04 21:53:05 +0545

    adding doPost method for registering student along with JDBC

--- StudentManagement/src/main/java/servlets/AddStudentServlet.java
+++ StudentManagement/src/main/java/servlets/AddStudentServlet.java
@@ -1,6 +1,12 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,11 +18,55 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(asyncSupported = true, urlPatterns = { "/registerstudent" })
 public class AddStudentServlet extends HttpServlet {
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		String studName = (String) request.getParameter("studName");
-		String studEmail = (String) request.getParameter("studEmail");
-		response.getWriter().append("<h1>Served at: </h1>").append(request.getContextPath());
+	private static final long serialVersionUID = 1L;
+	private PrintWriter writer; //to print in web browser
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("<h1>Served at:</h1> ").append(request.getContextPath());
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		writer = resp.getWriter();
+		// Data Retrieve
+		String username = req.getParameter("studName");
+		String email = req.getParameter("studEmail");
+		
+		// Database Connection & Query Execute
+		try {
+			// Driver Name
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			// Credentials
+			String localhostUrl = "jdbc:mysql://localhost:3306/student_management";
+			String localhostUsername = "root";
+			String localhostPassword = "";
+			
+			// DB connection
+			Connection connection = DriverManager.getConnection(localhostUrl, localhostUsername, localhostPassword);
+			
+			// Query -> Connection
+			String registerStudentQuery = "INSERT INTO student_info (username, email) VALUES(?, ?)";
+			PreparedStatement stmt = connection.prepareStatement(registerStudentQuery);
+			stmt.setString(1, username);
+			stmt.setString(2, email);
+			
+			// Run Query
+			int result = stmt.executeUpdate();
+			
+			if(result>0) {
+				// Success
+				writer.append("Successful!");
+			}else {
+				// Failed
+				writer.append("Unsuccessful!");
+			}
+			
+		}catch(ClassNotFoundException | SQLException ex) {
+			writer.append(((Throwable) ex).getMessage());
+		}
+		
 	}
 
 }
